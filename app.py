079469d948228e649b297f1b035e773c0362acea
JamesGBingham <49209198+JamesGBingham@users.noreply.github.com>
2021-10-18 15:04:13 +0100

    Updte add Colours

--- app.py
+++ app.py
@@ -2,5 +2,14 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+   HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 def hello():
-    return "Aloha, Ohana!"
+    return (bcolors.WARNING + "Aloha, Ohana!" + bcolors.ENDC)
